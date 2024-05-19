650ba6f6b2e2f189f1c8628a6ff7f308a12cff97
sgsm74 <saeed.razii93@gmail.com>
2024-05-19 10:10:53 +0330

    chore: di

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:idegostar/core/services/http_service.dart';
 import 'package:idegostar/features/authentication/data/datasources/remote_datasource/authentication_remote_datasource.dart';
 import 'package:idegostar/features/authentication/data/repositories/authentication_repository_impl.dart';
 import 'package:idegostar/features/authentication/domain/repositories/authentication_repository.dart';
@@ -22,10 +24,21 @@ Future<void> init() async {
 
   //repository
   sl.registerLazySingleton<AuthenticationRepository>(
-    () => AuthenticationRepositoryImpl(),
+    () => AuthenticationRepositoryImpl(authenticationRemoteDataSource: sl()),
   );
   //datasources
   sl.registerLazySingleton<AuthenticationRemoteDataSource>(
-    () => AuthenticationRemoteDataSourceImpl(),
+    () => AuthenticationRemoteDataSourceImpl(service: sl()),
+  );
+  //http service
+  sl.registerLazySingleton<HTTPService>(
+    () => DioService(
+      dio: Dio(
+        BaseOptions(
+          baseUrl: 'https://api.test',
+          receiveTimeout: const Duration(seconds: 50),
+        ),
+      ),
+    ),
   );
 }
