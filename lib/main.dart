66ad0b9e3f8007c0e680d4cd9d8aa4d55285f7a8
sgsm74 <saeed.razii93@gmail.com>
2024-05-19 10:11:48 +0330

    chore: add dio to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:idegostar/features/authentication/presentation/pages/login_page.dart';
 import 'package:idegostar/features/authentication/presentation/pages/signup_page.dart';
+import 'package:idegostar/injection_container.dart' as di;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await di.init();
   runApp(const MyApp());
 }
 
